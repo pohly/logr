ca15dec7dec08acec5226d4ea472088d16856ba6
Patrick Ohly <patrick.ohly@intel.com>
2021-09-13 09:46:37 +0200

    RFC: allow objects to marshal themselves
    This is more efficient than reflection-based marshalling and enables objects to
    decide for themselves which fields they want to log.

--- logr.go
+++ logr.go
@@ -477,3 +477,54 @@ type CallStackHelperLogSink interface {
 	// call site information.
 	GetCallStackHelper() func()
 }
+
+// MarshalLogObject is an optional interface that values in a key/value list
+// may implement. It allows the efficient logging of structured objects without
+// relying on reflection. It can be used in wrappers around other, larger
+// objects to select which fields should get logged.
+//
+// Loggers are not required to support it. Depending on their purpose,
+// they might prefer to use the Stringer interface, if available, for
+// example when the goal is to provide human-readable output.
+type ObjectMarshaler interface {
+	MarshalLogObject(ObjectEncoder) error
+}
+
+// ObjectEncoder provides methods for serializing structured data. Only loggers
+// that want to use ObjectMarshaler have to implement it.
+type ObjectEncoder interface {
+	// Logging-specific marshalers.
+	// AddArray(key string, marshaler ArrayMarshaler) error
+	// AddObject(key string, marshaler ObjectMarshaler) error
+
+	// Built-in types.
+	// AddBinary(key string, value []byte)     // for arbitrary bytes
+	// AddByteString(key string, value []byte) // for UTF-8 encoded bytes
+	// AddBool(key string, value bool)
+	// AddComplex128(key string, value complex128)
+	// AddComplex64(key string, value complex64)
+	// AddDuration(key string, value time.Duration)
+	// AddFloat64(key string, value float64)
+	// AddFloat32(key string, value float32)
+	// AddInt(key string, value int)
+	// AddInt64(key string, value int64)
+	// AddInt32(key string, value int32)
+	// AddInt16(key string, value int16)
+	// AddInt8(key string, value int8)
+	AddString(key, value string)
+	// AddTime(key string, value time.Time)
+	// AddUint(key string, value uint)
+	// AddUint64(key string, value uint64)
+	// AddUint32(key string, value uint32)
+	// AddUint16(key string, value uint16)
+	// AddUint8(key string, value uint8)
+	// AddUintptr(key string, value uintptr)
+
+	// AddReflected uses reflection to serialize arbitrary objects, so it can be
+	// slow and allocation-heavy.
+	//AddReflected(key string, value interface{}) error
+	// OpenNamespace opens an isolated namespace where all subsequent fields will
+	// be added. Applications can use namespaces to prevent key collisions when
+	// injecting loggers into sub-components or third-party libraries.
+	// OpenNamespace(key string)
+}
